60c7d165b03c613a6ba3dc3b07c31a363fe62a8c
Nikita Ibragimov <nik.ibragimov@gmail.com>
2017-02-27 21:57:14 -0600

    Now work from any folder
    Now work from any folder

--- Pars_Bashrc.py
+++ Pars_Bashrc.py
@@ -1,7 +1,7 @@
 #For Linux ofc.
 #file should be in "~/"
 lines = []    # Will store all bash lines in this list
-with open(".bashrc", 'r') as f:   # Openning and reading file, could be any other custom file with your alias
-    for line in f: lines.append(line.rstrip())  # All string -> in list withou "\n"
-for line in lines[lines.index("#:::")+2:]:    # printing custom alias which must go AFTER string "#:::"
+with open("/home/ser/.bashrc", 'r') as f:   # Openning and reading file, could $
+    for line in f: lines.append(line.rstrip())  # All string -> in list withou $
+for line in lines[lines.index("#:::")+2:]:    # printing custom alias which mus$
     print (line)
